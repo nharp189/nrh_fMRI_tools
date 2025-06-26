bcff3a87f8ed0b61b7669fde631b099363c03c42
Nicholas Harp <nharp@huskers.unl.edu>
2025-06-26 13:23:20 -0700

    add fs projection bash script

---
+++ fs5_proj_from_vol.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for hemi in lh rh; do
+  for vol in /Users/nicholasharp/Desktop/spintests_v2/ns_fi_FWE_height/*.nii; do
+    name=$(basename "$vol" .nii)
+    mri_vol2surf \
+      --mov "$vol" \
+      --hemi $hemi \
+      --o fs5_mids_height/${hemi}.${name}_fs5.mgh \
+      --surfreg $SUBJECTS_DIR/fsaverage5 \
+      --regheader fsaverage5 \
+      --projfrac 0.5 \
+      --interp nearest \
+      --mni152reg 
+  done
+done
